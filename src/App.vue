6a910c530c2f670b45e4175c078955d8bd1af1f9
Geoo Chi <geoooochi@gmail.com>
2024-10-31 18:51:17 +0800

    fix base path

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <!-- Sidebar navigation with fixed header and scrollable content -->
     <nav class="flex w-64 flex-col border-r bg-gray-50">
       <!-- Fixed Header -->
-      <RouterLink to="/">
+      <RouterLink to="/spark-ui-examples">
         <div class="border-b p-4">
           <h1 class="text-xl font-bold text-gray-800">Spark UI examples</h1>
         </div>
@@ -44,28 +44,28 @@ export default {
   data() {
     return {
       routes: [
-        { path: '/animatedBeam', name: 'animatedBeam' },
-        { path: '/animatedGradientText', name: 'animatedGradientText' },
-        { path: '/animatedList', name: 'animatedList' },
-        { path: '/animatedShinyText', name: 'animatedShinyText' },
-        { path: '/avatarCircle', name: 'avatarCircle' },
-        { path: '/bento', name: 'bento' },
-        { path: '/blurFade', name: 'blurFade' },
-        { path: '/blurIn', name: 'blurIn' },
-        { path: '/dotPattern', name: 'dotPattern' },
-        { path: '/globe', name: 'globe' },
-        { path: '/gradualSpacing', name: 'gradualSpacing' },
-        { path: '/letterUp', name: 'letterUp' },
-        { path: '/marquee', name: 'marquee' },
-        { path: '/marquee3D', name: 'marquee3D' },
-        { path: '/marqueeVertical', name: 'marqueeVertical' },
-        { path: '/meteors', name: 'meteors' },
-        { path: '/orbitingCircles', name: 'orbitingCircles' },
-        { path: '/particles', name: 'particles' },
-        { path: '/retroGrid', name: 'retroGrid' },
-        { path: '/ripple', name: 'ripple' },
-        { path: '/skewedInfiniteScroll', name: 'skewedInfiniteScroll' },
-        { path: '/typingAnimation', name: 'typingAnimation' },
+        { path: '/spark-ui-examples/animatedBeam', name: 'animatedBeam' },
+        { path: '/spark-ui-examples/animatedGradientText', name: 'animatedGradientText' },
+        { path: '/spark-ui-examples/animatedList', name: 'animatedList' },
+        { path: '/spark-ui-examples/animatedShinyText', name: 'animatedShinyText' },
+        { path: '/spark-ui-examples/avatarCircle', name: 'avatarCircle' },
+        { path: '/spark-ui-examples/bento', name: 'bento' },
+        { path: '/spark-ui-examples/blurFade', name: 'blurFade' },
+        { path: '/spark-ui-examples/blurIn', name: 'blurIn' },
+        { path: '/spark-ui-examples/dotPattern', name: 'dotPattern' },
+        { path: '/spark-ui-examples/globe', name: 'globe' },
+        { path: '/spark-ui-examples/gradualSpacing', name: 'gradualSpacing' },
+        { path: '/spark-ui-examples/letterUp', name: 'letterUp' },
+        { path: '/spark-ui-examples/marquee', name: 'marquee' },
+        { path: '/spark-ui-examples/marquee3D', name: 'marquee3D' },
+        { path: '/spark-ui-examples/marqueeVertical', name: 'marqueeVertical' },
+        { path: '/spark-ui-examples/meteors', name: 'meteors' },
+        { path: '/spark-ui-examples/orbitingCircles', name: 'orbitingCircles' },
+        { path: '/spark-ui-examples/particles', name: 'particles' },
+        { path: '/spark-ui-examples/retroGrid', name: 'retroGrid' },
+        { path: '/spark-ui-examples/ripple', name: 'ripple' },
+        { path: '/spark-ui-examples/skewedInfiniteScroll', name: 'skewedInfiniteScroll' },
+        { path: '/spark-ui-examples/typingAnimation', name: 'typingAnimation' },
       ],
     }
   },
