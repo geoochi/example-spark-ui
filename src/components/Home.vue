410f2042f9173221b0ff75cf04040ed1df5c82a5
Geoo Chi <geoooochi@gmail.com>
2024-10-31 17:32:23 +0800

    update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
-    <h1 class="text-2xl font-bold mb-4">Home Page</h1>
+    <h1 class="text-2xl font-bold mb-4">
+      Reference repo 👉
+      <a
+        class="underline text-blue-500"
+        href="https://spark-ui.dev/"
+        target="_blank"
+        >Spark UI</a
+      >
+    </h1>
   </div>
 </template>
