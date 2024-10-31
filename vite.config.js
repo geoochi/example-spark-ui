377d4028225d70ade26aff91ff1ed9c42df7ec83
Geoo Chi <geoooochi@gmail.com>
2024-10-31 18:37:14 +0800

    add base

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/spark-ui-examples',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
